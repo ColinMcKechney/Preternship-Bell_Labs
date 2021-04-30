ccdccd0b4970e169d89bc6ee88194bc8c17bc097
Ian Aponte <ianaponte@dhcp-10-7-248-0.dhcp.nd.edu>
2021-04-30 13:27:11 -0400

    Added Priority and Name as well as a << operator

--- Node.h
+++ Node.h
@@ -2,55 +2,75 @@
 #define NODE_H
 
 #include <cstdlib>	
+#include <iostream>
 
+
+
+enum NetworkSliceType{ //Keep Consistency with edge.h
+
+	NoSlice = 0,
+	Broadband = 1,
+	MissionCritical = 2,
+
+};
 template< class T >
 struct node
 {
-	T     data;
-	node* next;
-	node* prev;
+	private:
+
+		T     data;
+		node* next;
+		node* prev;
+		int name; // labels which "edge cloud" it is
+		int priority; // elaborates which network slice type it is
+	public:
+
 	
-	node() : data(), next(NULL), prev(NULL) { }
+		node() : data(), next(NULL), prev(NULL), name(), priority() { }
 	
-	node(const T& dataIn) : data(dataIn), next(NULL), prev(NULL) { }
+		node(const T& dataIn, const int nameIn, const int priorityIn) : data(dataIn), next(NULL), prev(NULL), name(nameIn), priority(priorityIn) { }
 	
-	~node(){
-		
-		delete next;
+		~node(){
+			delete next;
 		
 	}
 	
 
-	node(const node<T>& copy) : data( copy.data ), next( NULL ), prev( NULL ) { }
+		node(const node<T>& copy) : data( copy.data ), next( NULL ), prev( NULL ), name(copy.name), priority(copy.priority) { }
 	
 	
-	node<T>& operator=(const node<T>& assign){
+		node<T>& operator=(const node<T>& assign){
 		
-		if(this != &assign){
-			this->data = assign.data;
-			this->next = NULL;
-			this->prev = NULL;
+			if(this != &assign){
+				this->data = assign.data;
+				this->next = NULL;
+				this->prev = NULL;
+			}
+			return *this;
 		}
-		return *this;
-	}
 	
 	
-	node<T>* operator=(const node<T>* assign){
+		node<T>* operator=(const node<T>* assign){
 		
-		if(this != (void *)&assign){
-			this->data = assign->data;
-			this->next = NULL;
-			this->prev = NULL;
+			if(this != (void *)&assign){
+				this->data = assign->data;
+				this->next = NULL;
+				this->prev = NULL;
+			}
+			return *this;
 		}
-		return *this;
-	}
 	
 	
-	bool operator!=(const node<T>* rhs){
+		bool operator!=(const node<T>* rhs){
 		
-		return this != (void *)&rhs;
+			return this != (void *)&rhs;
 		
-	}
+		}
+
+		friend std::ostream& operator<<(std::ostream& output, node& nodeIn){
+			output << "Data: "<< nodeIn.data<<"Name: "<<nodeIn.name<< " Priority: "<<nodeIn.priority<<std::endl;	
+			return output;
+		}
 	
 };
 
