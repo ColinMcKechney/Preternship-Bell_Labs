9718aa934064dd9cc94b2784dfe540b56fcee8dc
Colin McKechney <colin@mckechney.com>
2021-04-30 11:00:55 -0400

    changed param map to string/int

--- inc/Edge.h
+++ inc/Edge.h
@@ -4,6 +4,7 @@
 #include "Node.h"
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include <unordered_map>
 
 //Used to define the different newtwork slice types to identify which optimization method should be used
@@ -30,7 +31,7 @@ struct Edge
 	private:
 		
 		NetworkSliceType sliceType; //Identifies how the weight is calculated
-		std::unordered_map<int, double> param; //Array of parameters to optimize the network slice
+		std::unordered_map<std::string, int> param; //Array of parameters to optimize the network slice
 		double weight; //Stores the weight so it isn't recalculated upon calling each time
 		
 		//Default funciton for error catching
@@ -43,11 +44,11 @@ struct Edge
 			//needs proper investigation once simulation is completed 
 		double weight_Broadband()
 		{
-			return param[ParameterType::Latency];
+			return param["delay"];
 		}
 		double weight_MissionCritical()
 		{
-			return param[ParameterType::Latency] + param[ParameterType::Reliability];
+			return param["reliability"] + param["packet_loss"];
 		}
 		
 	public:
@@ -58,9 +59,9 @@ struct Edge
 		Edge() : sliceType(NetworkSliceType::NoSlice), param(), weight(), begining(NULL), destination(NULL) {}
 		
 		//Constructors, allowing for multiple overloads but upon implementation, one will be chosed
-		Edge(const int type_in, const std::unordered_map<int, double> param_in, Node* start = NULL, Node* end = NULL) : sliceType((NetworkSliceType)type_in), param(param_in), weight(), begining(start), destination(end) {getWeight(sliceType);}
-		Edge(const NetworkSliceType type_in, const std::unordered_map<int, double> param_in, Node* start = NULL, Node* end = NULL) : sliceType(type_in), param(param_in), weight(), begining(start), destination(end) {getWeight(sliceType);} //Prefered to use for implementation
-		Edge(const int type_in, const double param_in[], Node* start = NULL, Node* end = NULL) : sliceType((NetworkSliceType)type_in), param(), weight(), begining(start), destination(end) {
+		Edge(const int type_in, const std::unordered_map<std::string, int> param_in, Node* start = NULL, Node* end = NULL) : sliceType((NetworkSliceType)type_in), param(param_in), weight(), begining(start), destination(end) {getWeight(sliceType);}
+		Edge(const NetworkSliceType type_in, const std::unordered_map<std::string, int> param_in, Node* start = NULL, Node* end = NULL) : sliceType(type_in), param(param_in), weight(), begining(start), destination(end) {getWeight(sliceType);} //Prefered to use for implementation
+		/*Edge(const int type_in, const double param_in[], Node* start = NULL, Node* end = NULL) : sliceType((NetworkSliceType)type_in), param(), weight(), begining(start), destination(end) {
 			for(int i = 0; i < numParameters; i++)
 			{
 				param[i] = param_in[i];
@@ -73,7 +74,7 @@ struct Edge
 				param[i] = param_in[i];
 			}
 			getWeight(sliceType);
-		}
+		}*/
 		
 		//Simple accessor of weight variable
 		double getWeight() const
@@ -103,7 +104,7 @@ struct Edge
 		//Output overload
 		friend std::ostream& operator<<(std::ostream& output, Edge& edge_in)
 		{
-			for(std::pair<int, double> element : edge_in.param)
+			for(std::pair<std::string, int> element : edge_in.param)
 			{
 				output<<"Parameter: "<<element.first<<" - Value: "<<element.second<<std::endl;
 			}
